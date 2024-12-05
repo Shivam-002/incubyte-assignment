e9f7347e49fea342a93b6b7ccf74d77b116d389c
Shivam <shivamprajapati035@gmail.com>
2024-12-05 13:09:30 +0530

    Added Tests for any amount of number

--- main.test.ts
+++ main.test.ts
@@ -12,4 +12,16 @@ describe("add_strings", () => {
   it("should return the sum of two numbers", () => {
     expect(add_strings("1,2")).toBe(3);
   });
+
+  it("should return the sum of three numbers", () => {
+    expect(add_strings("1,2,3")).toBe(6);
+  });
+
+  it("should return the sum of four numbers", () => {
+    expect(add_strings("1,2,3,4")).toBe(10);
+  });
+
+  it("should return the sum of ten numbers", () => {
+    expect(add_strings("1,2,3,4,5,6,7,8,9,10")).toBe(55);
+  });
 });
