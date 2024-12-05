4d4fe4f6e716e596161d1d4017ea658fc0d5df58
Shivam <shivamprajapati035@gmail.com>
2024-12-05 12:54:53 +0530

    Added empty string test

---
+++ main.test.ts
@@ -0,0 +1,9 @@
+const { add_strings } = require("./main");
+
+describe("add_strings", () => {
+  it("should return 0 for an empty string", () => {
+    expect(add_strings("")).toBe(0);
+  });
+});
+
+describe;
