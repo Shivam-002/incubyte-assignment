f30ff853f6fb41bc6e1fc5884d9313cb6d7517c6
Shivam <shivamprajapati035@gmail.com>
2024-12-05 13:10:51 +0530

    Improved the function to handle any amount of number.

--- main.ts
+++ main.ts
@@ -1,14 +1,10 @@
 const add_strings = (numbers: string): number => {
-  //Empty string
-  if (numbers.length === 0) return 0;
+  // If the string is empty, return 0
+  if (numbers === "") {
+    return 0;
+  }
 
-  const num_arr = numbers.split(",");
-
-  //Single number
-  if (num_arr.length === 1) return parseInt(num_arr[0]);
-
-  //Two numbers
-  return parseInt(num_arr[0]) + parseInt(num_arr[1]);
+  return numbers.split(",").reduce((acc, num) => acc + parseInt(num, 10), 0);
 };
 
 export { add_strings };
