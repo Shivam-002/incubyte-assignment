ad184f6b7e0684fd9249c6b8ddf8ae287d3e9d64
Shivam <shivamprajapati035@gmail.com>
2024-12-05 13:07:13 +0530

    Added Basic Test and Implementation

--- main.test.ts
+++ main.test.ts
@@ -4,6 +4,12 @@ describe("add_strings", () => {
   it("should return 0 for an empty string", () => {
     expect(add_strings("")).toBe(0);
   });
-});
 
-describe;
+  it("should return 1 for a single number", () => {
+    expect(add_strings("1")).toBe(1);
+  });
+
+  it("should return the sum of two numbers", () => {
+    expect(add_strings("1,2")).toBe(3);
+  });
+});
--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
 const add_strings = (numbers: string): number => {
-  return NaN; // Stub implementation
+  //Empty string
+  if (numbers.length === 0) return 0;
+
+  const num_arr = numbers.split(",");
+
+  //Single number
+  if (num_arr.length === 1) return parseInt(num_arr[0]);
+
+  //Two numbers
+  return parseInt(num_arr[0]) + parseInt(num_arr[1]);
 };
 
 export { add_strings };
