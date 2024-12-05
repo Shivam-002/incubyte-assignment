64f509b9f83f3815e59a47c1fa4f97447b0623cd
Shivam <shivamprajapati035@gmail.com>
2024-12-05 13:14:17 +0530

    Added Newline Addition Tests

--- main.test.ts
+++ main.test.ts
@@ -24,4 +24,12 @@ describe("add_strings", () => {
   it("should return the sum of ten numbers", () => {
     expect(add_strings("1,2,3,4,5,6,7,8,9,10")).toBe(55);
   });
+
+  it("should return the sum of numbers separated by newlines", () => {
+    expect(add_strings("1\n2,3")).toBe(6);
+  });
+
+  it("should return the sum of numbers separated by newlines", () => {
+    expect(add_strings("1\n2\n3")).toBe(6);
+  });
 });
